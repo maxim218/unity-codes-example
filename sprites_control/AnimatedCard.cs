25c722d4eeca57c0707dd5a5a41bb78f1c4fc208
maxim218 <31542700+maxim218@users.noreply.github.com>
2021-01-09 22:40:04 +0300

    Sprites

---
+++ sprites_control/AnimatedCard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedCard : MonoBehaviour {
+    [SerializeField] private Sprite cardA = null;
+    [SerializeField] private Sprite cardB = null;
+    [SerializeField] private Sprite cardC = null;
+
+    private int _count = 0;
+    
+    public void NextImage() {
+        SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        /////
+        if(_count == 0) spriteRenderer.sprite = cardA;
+        if(_count == 1) spriteRenderer.sprite = cardB;
+        if(_count == 2) spriteRenderer.sprite = cardC;
+        /////
+        _count++;
+        _count %= 3;
+    }
+}
---
+++ sprites_control/CardControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ImgType {
+    A,
+    B,
+    C
+}
+
+public class CardControl : MonoBehaviour {
+    [SerializeField] private Sprite cardA = null;
+    [SerializeField] private Sprite cardB = null;
+    [SerializeField] private Sprite cardC = null;
+
+    [SerializeField] private ImgType type;
+
+    void OnMouseDown() {
+        SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        if(type == ImgType.A) spriteRenderer.sprite = cardA;
+        if(type == ImgType.B) spriteRenderer.sprite = cardB;
+        if(type == ImgType.C) spriteRenderer.sprite = cardC;
+    }
+}
