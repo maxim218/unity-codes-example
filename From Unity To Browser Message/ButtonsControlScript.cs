04e4ab923a4b9fb11ca114065dc8714053d771d7
maxim218 <31542700+maxim218@users.noreply.github.com>
2021-02-23 23:10:27 +0300

    From Unity To Brower Message

---
+++ From Unity To Browser Message/ButtonsControlScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsControlScript : MonoBehaviour {
+    void OnGUI() {
+        if (GUI.Button(new Rect(50, 50, 50, 50), "A")) MySendToBrowser('A');
+        if (GUI.Button(new Rect(150, 50, 50, 50), "B")) MySendToBrowser('B');
+        if (GUI.Button(new Rect(250, 50, 50, 50), "C")) MySendToBrowser('C');
+    }
+
+    void MySendToBrowser(char c) {
+        string functionString = "myJavaScriptFunc";
+        string messageString = "aaa_" + c + "_bbb";
+        Application.ExternalCall(functionString, messageString);
+    }
+}
