02ef28eda539ecdf0e5d5305ba311d06dadd508f
maxim218 <maxim.kolotovkin@yandex.ru>
2021-08-25 21:04:36 +0300

    My GetComponent

---
+++ My GetComponent/BallControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+    [SerializeField] private Vector2 vectorMoving = Vector2.zero;
+
+    private Rigidbody2D _rigidbody2D = null;
+
+    private void Start() {
+        _rigidbody2D = GetterComponent.GetComp<Rigidbody2D>(gameObject);
+    }
+
+    private void FixedUpdate() {
+        _rigidbody2D.velocity = vectorMoving;
+    }
+}
---
+++ My GetComponent/GetterComponent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GetterComponent {
+    public static T GetComp <T> (GameObject obj) {
+        T comp = obj.GetComponent<T>();
+        return comp;
+    }
+}
