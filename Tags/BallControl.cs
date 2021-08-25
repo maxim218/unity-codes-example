0d8337bfd5e3fe0510cb916bc8256baf6d04848f
maxim218 <maxim.kolotovkin@yandex.ru>
2021-08-25 18:39:50 +0300

    Tags

---
+++ Tags/BallControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+    private static void RedMethod() {
+        const string msg = "Red";
+        Debug.Log(msg);
+    }
+
+    private static void GreenMethod() {
+        const string msg = "Green";
+        Debug.Log(msg);
+    }
+
+    private static void BlueMethod() {
+        const string msg = "Blue";
+        Debug.Log(msg);
+    }
+    
+    private void OnMouseDown() {
+        string myTag = gameObject.tag;
+        switch (myTag) {
+            case "TagRed": RedMethod(); break;
+            case "TagGreen": GreenMethod(); break;
+            case "TagBlue": BlueMethod(); break;
+        }
+    }
+}
---
+++ Tags/MainController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour {
+    private GameObject [] _redArray = null;
+    private GameObject [] _greenArray = null;
+    private GameObject [] _blueArray = null;
+
+    private void Start() {
+        _redArray = GameObject.FindGameObjectsWithTag("TagRed");
+        _greenArray = GameObject.FindGameObjectsWithTag("TagGreen");
+        _blueArray = GameObject.FindGameObjectsWithTag("TagBlue");
+    }
+
+    private static void MoveElements(GameObject [] arr, float speedX, float speedY) {
+        if (arr == null) return;
+        if (arr.Length == 0) return;
+        foreach (GameObject element in arr) {
+            const int zero = 0;
+            element.transform.Translate(speedX * Time.deltaTime, speedY * Time.deltaTime, zero);
+        }
+    }
+   
+    private void Update() {
+        MoveElements(_redArray, -2, 0);
+        MoveElements(_greenArray, 0, 1);
+        MoveElements(_blueArray, 1, 0);
+    }
+}
