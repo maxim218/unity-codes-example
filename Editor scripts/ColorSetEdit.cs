d754db3879298b61a04d72d9e2ec77d044bf5d09
m.kolotovkin <m.kolotovkin@corp.mail.ru>
2021-10-25 14:33:03 +0300

    Editor

---
+++ Editor scripts/ColorSetEdit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+
+namespace Editor
+{
+    [CustomEditor(typeof(GameObject))]
+    public class ColorSetEdit : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUILayout.BeginHorizontal();
+              if (GUILayout.Button("Red")) SetColor(Color.red);
+              if (GUILayout.Button("Green")) SetColor(Color.green);
+              if (GUILayout.Button("Blue")) SetColor(Color.blue);
+            GUILayout.EndHorizontal();
+        }
+
+        private void SetColor(Color color)
+        {
+            GameObject me = (GameObject) target;
+            Renderer comp = me.GetComponent<Renderer>();
+            comp.sharedMaterial.color = color;
+        }
+    }
+}
