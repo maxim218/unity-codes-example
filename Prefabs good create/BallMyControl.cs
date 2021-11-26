8aae4edd57dfddf745437a81ea296a0cb06f8df3
m.kolotovkin <m.kolotovkin@corp.mail.ru>
2021-11-26 13:09:31 +0300

    Prefabs good create

---
+++ Prefabs good create/BallMyControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMyControl : MonoBehaviour
+{
+    private float _speed = 0;
+
+    public void SetSpeed(float speedValue)
+    {
+        _speed = speedValue;
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        transform.position = position;
+    }
+    
+    private void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Prefabs good create/MyTestScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MyTestScript : MonoBehaviour
+{
+    [SerializeField] private BallMyControl ballMyControl = null;
+    
+    private void Update()
+    {
+        const float speedMove = 1.0f;
+        transform.Translate(speedMove * Time.deltaTime, 0, 0);
+    }
+    
+    private void OnMouseDown()
+    {
+        BallMyControl script = Instantiate(ballMyControl);
+
+        Vector3 position = transform.position;
+        position.y -= 1.5f;
+        script.SetPosition(position);
+
+        const float speed = -1.1f;
+        script.SetSpeed(speed);
+    }
+}
